fedaff3857926fc2925d335509d11bacb942033c
Benson <bensongathu23@gmail.com>
2021-05-29 15:13:13 +0300

    User class created

---
+++ user.py
@@ -0,0 +1,14 @@
+# class User:
+#     """A class for creating users"""
+#     users = {}
+#     def __init__(self,username,password):
+#         self.username = username
+#         self.password = password
+
+#     def save_user(self,key,value):
+#         """method that saves a created user"""
+#         User.users[key] = value
+
+#     @classmethod
+#     def login(cls,username):
+#         pass
---
+++ user_test.py
@@ -0,0 +1,19 @@
+from user import User
+import unittest
+
+class TestUser(unittest.TestCase):
+    """
+    Test Class that tests methods present in our Userclass
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases.
+    """
+    def setUp(self):
+        """setup method to run before all other test casses"""
+        self.new_user = User("Benson","vcxz4321")
+    
+    def tearDown(self):
+        """tearDown method that does clean up after each test case has run"""
+        pass
+
+    
+
