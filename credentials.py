af1752cd2bfb3b1005f097dbcebfd9ec6be57949
Benson <bensongathu23@gmail.com>
2021-05-29 16:05:19 +0300

    save credentials method created

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@ import pyperclip
 
 class Credentals:
     #class that generates new instances of credentials
-    credentials = []
+    credential_list = []
     def __init__(self,account,username,password):
         self.account = account
         self.username = username
@@ -10,3 +10,6 @@ class Credentals:
 
     def save_creds(self):
         """method that saves created credentials"""
+        Credentals.credential_list.append(self)
+
+    
