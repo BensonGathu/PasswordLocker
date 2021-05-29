20498ca8b7893f5c116a8c58638d39f99249bc93
Benson <bensongathu23@gmail.com>
2021-05-29 16:39:32 +0300

    methods to delete credentials,search account,check if account exists, display and coppy credentials created

--- credentials.py
+++ credentials.py
@@ -11,5 +11,45 @@ class Credentals:
     def save_creds(self):
         """method that saves created credentials"""
         Credentals.credential_list.append(self)
-
     
+    def delete_creds(self)
+        """method that deletes credentials from our credentials list"""
+        Credentals.credential_list.remove(self)
+
+    @classmethod
+    def search_by_account(cls,account):
+        """
+        method that takes in an account name and returns a account username and password that matches that account name.
+        Args:
+            account: Account name  to search for.
+        REturns:
+            Details of account  that matches the name.
+        """
+        for credentials in cls.credential_list:
+            if credentials.account == account:
+                return credentials
+
+    @classmethod
+    def credential_exist(cls,account):
+          """
+        method that checks if an account exists from the credentials_list. 
+        Args:
+            account: Account name to search if it exists
+        Returns:
+            Boolean: True or false depending if the credentials exists
+        """
+        for credentials in cls.credentials_list:
+            if credentials.account == account:
+                return True
+            return False
+
+    @classmethod
+    def display_credentials(cls):
+        """method that returns all my existing credentials"""
+        return cls.credential_list
+    
+    @classmethod
+    def copy_credentials(cls,account):
+        """method that will copy an accounts credentials"""
+        account_found = search_by_account(account)
+        pyperclip.copy(account_found.username,account_found.password)
